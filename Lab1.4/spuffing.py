f6957e8738c966a0f93433be171916f158c16581
Vova <hspvova@gmail.com>
2024-01-19 01:31:23 +0300

    spuffing

---
+++ Lab1.4/spuffing.py
@@ -0,0 +1,22 @@
+import random
+from ipaddress import IPv4Network
+
+
+class Spuffer(IPv4Network):
+    def __init__(self):
+        IPv4Network.__init__(self, (random.randint(0x0b000000, 0xdf000000), random.randint(8, 24)), strict=False)
+
+def key_sort(network):
+    return int(network.netmask)* 2**32 + int(network.network_address)
+
+def gen_pool(i):
+    gen = [Spuffer() for i in range(i)]
+    return sorted(gen, key=(key_sort))
+
+
+net1 = gen_pool(50)
+
+for i in net1:
+    print(i)
+
+# print(net1)
