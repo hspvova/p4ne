9a3c349ae11979609ad0ae9f9c46dd4122726125
Vova <hspvova@gmail.com>
2024-01-20 04:39:53 +0300

    find_ip

--- Lab1.6/findip.py
+++ Lab1.6/findip.py
@@ -4,12 +4,13 @@ from ipaddress import IPv4Interface
 
 
 def find_ip(string):
-    ip_plus_netmask = re.findall(r"((?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{2}|[0-9])(?:\.?|$)){4})", string)
-    res = 0
-    if len(ip_plus_netmask) == 2:
-        ip_addr = ip_plus_netmask[0]+'/'+ip_plus_netmask[1]
-        res = IPv4Interface(ip_addr)
-    return res
+    if "ip address" in string:
+        ip_plus_netmask = re.findall(r"((?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{2}|[0-9])(?:\.?|$)){4})", string)
+        if len(ip_plus_netmask) == 2:
+            ip_addr = ip_plus_netmask[0]+'/'+ip_plus_netmask[1]
+            res = IPv4Interface(ip_addr)
+            return res
+    return
 
 
 result = []
@@ -17,10 +18,9 @@ result = []
 for filename in glob.glob(r"..\..\config_files\*.log"):
     with (open(filename) as fil):
         for string in fil:
-            if "ip address" in string:
-                tango = find_ip(string.strip())
-                if tango != 0:
-                    result.append(tango)
+            tango = find_ip(string.strip())
+            if tango is not None:
+                result.append(tango)
 
 s_old = ""
 
